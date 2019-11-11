7945956764fa72e9c187b3c90342570fc3072cff
kimd0nghun <37896242+kimd0nghun@users.noreply.github.com>
2019-11-11 21:24:33 +0900

    delete

---
+++ src/BAEKJOON/B_11721.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+import java.util.*;
+public class B_11721 {
+
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String N =sc.nextLine();
+		int check = 0;
+		int t=0;
+		char [] A = new char [N.length()];
+		for(int i =0; i<N.length(); i++) {
+			A[i] = N.charAt(i);
+			check++;
+			System.out.print(A[i]);
+			if(check >9 && check%10 == 0) {
+				
+				System.out.println();
+			}
+		}
+		System.out.println(check);
+		
+		
+	}
+}
