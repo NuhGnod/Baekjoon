12d7da40121f05630360620a14a696705645bb43
kimd0nghun <37896242+kimd0nghun@users.noreply.github.com>
2019-11-18 21:54:53 +0900

    B_9461
    BaekJoon Online Judge

--- src/B_9461/Main.java
+++ src/B_9461/Main.java
@@ -8,15 +8,15 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		int dp[] = new int[T];
+		long dp[] = new long[T];
 		// 규칙 -- 바로 전 even[n] = odd중 odd[n-1]빼고 모두 더한 값 +1
 		// 1 1 2 4 7 //odd
 		// 0 1 2 3 5 9 //even
 		for (int t = 0; t < T; t++) {
 			int N = sc.nextInt();
 
-			int odd[] = new int[N + 3];
-			int even[] = new int[N + 3];
+			long odd[] = new long[N + 3];
+			long even[] = new long[N + 3];
 			odd[1] = 1;
 			for (int i = 2, j = 1; i <= N + 1 && j <= N + 1; i += 2, j += 2) {
 				for (int q = 0; q <= i; q++) {
@@ -42,7 +42,7 @@ public class Main {
 
 		}
 		for(int t=0; t<T; t++) {
-			System.out.println(dp[t]);
+			System.out.println(dp[t]);	
 		}
 		
 
