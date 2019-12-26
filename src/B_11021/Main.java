9501368a4fbf9237a43fecc3aaa8c41984725dc8
kimd0nghun <37896242+kimd0nghun@users.noreply.github.com>
2019-12-26 23:12:53 +0900

    B_11021
    Baekjoon Online Judge

---
+++ src/B_11021/Main.java
@@ -0,0 +1,19 @@
+package B_11021;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int i=0; i<T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println("Case #" + (i+1)+": " + (A+B));
+		}
+	}
+
+}
