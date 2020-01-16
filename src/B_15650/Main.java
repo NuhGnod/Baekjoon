9afeae30ddee586cfc1fa7d1000fd55e39855390
donghun <zhfptm12@naver.com>
2020-01-16 14:15:15 +0900

    B_15651

--- src/B_15650/Main.java
+++ src/B_15650/Main.java
@@ -3,8 +3,8 @@ package B_15650;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated me	thod stub
+		System.out.println("dasd");
 	}
 
 }
