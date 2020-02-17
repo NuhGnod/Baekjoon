744dd3ff6d09bece2242faa81bcf8861b4634e31
donghun <zhfptm12@naver.com>
2020-02-18 00:01:04 +0900

    B_11866
    Queue

---
+++ src/B_11866/Main.java
@@ -0,0 +1,29 @@
+package B_11866;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		
+		int N = sc.nextInt();
+		int k = sc.nextInt();
+		for (int i = 1; i <= N; i++) {
+			q.add(i);
+		}
+		int cnt = 0;
+		int count = 0;
+		int arr[] = new int[N];
+		Iterator<Integer> iterator = q.iterator();
+
+		while (iterator.hasNext()) {
+			
+		}
+	}
+
+}
