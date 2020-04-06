c332462876ef2529e634f415ecd0ad4f252e98d7
donghun <zhfptm12@naver.com>
2020-04-06 21:51:06 +0900

    divide and conquer
    divide and conquer

--- src/B_10830/Main.java
+++ src/B_10830/Main.java
@@ -6,11 +6,13 @@ public class Main {
 	static long time = 1;
 	static long num = 0;
 
+	static int N = 0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		int B = sc.nextInt();
+		N = sc.nextInt();
+		long B = sc.nextLong();
 		long A[][] = new long[N][N];
 		long C[][] = new long[N][N];
 		num = B;
@@ -20,60 +22,48 @@ public class Main {
 
 			}
 		}
-		multiply(A, B, A);
-
-	}
-
-	static public void multiply(long a[][], int b, long c[][]) {
-		long C[][] = new long[a.length][a.length];
-
-		if (b == 1) {
-			for (int i = 0; i < a.length; i++) {
-				for (int j = 0; j < a.length; j++) {
-					for (int q = 0; q < a.length; q++) {
-						C[i][j] += c[i][q] * a[q][j] % 1000;
-					}
-				}
-			}
-			for (int i = 0; i < a.length; i++) {
-				for (int j = 0; j < a.length; j++) {
-					System.out.print(C[i][j] % 1000 + " ");
+		if (B == 1) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(A[i][j] % 1000 + " ");
 				}
 				System.out.println();
 			}
-
-		} else if (b % 2 == 1) {
-			for (int i = 0; i < a.length; i++) {
-				for (int j = 0; j < a.length; j++) {
-					for (int q = 0; q < a.length; q++) {
-						C[i][j] += c[i][q] * c[q][j] % 1000;
-					}
-				}
-			}
-			time *= 2;
-			if (num - time * ((b - 1) / 2) > 1) {
-				multiply(c, (b-1) / 2, C);
-
-			} else {
-				multiply(a, (b-1) / 2, C);
-			}
-
 		} else {
-			for (int i = 0; i < a.length; i++) {
-				for (int j = 0; j < a.length; j++) {
-					for (int q = 0; q < a.length; q++) {
-						C[i][j] += c[i][q] * c[q][j] % 1000;
-					}
+			C = matrix(A, B);
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					System.out.print(C[i][j] % 1000+ " ");
+				}
+				System.out.println();
+			}
+		}
+	}
+
+	static public long[][] mul(long a[][], long b[][]) {
+		long[][] C = new long[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				for (int q = 0; q < N; q++) {
+					C[i][j] += (a[i][q] * b[q][j]);
+					C[i][j] %= 1000;
 				}
 			}
-			time *= 2;
-			if (num - time * (b / 2) > 1) {
-				multiply(c, b / 2, C);
+		}
+		return C;
+	}
 
-			} else {
-				multiply(a, b / 2, C);
-			}
+	static public long[][] matrix(long a[][], long b) {
+		long C[][] = new long[a.length][a.length];
 
+		if (b == 1) {
+			return a;
+		} else if (b % 2 == 1) {
+			C = matrix(a, b - 1);
+			return mul(C, a);
+		} else {
+			C = matrix(a, b / 2);
+			return mul(C, C);
 		}
 
 	}
