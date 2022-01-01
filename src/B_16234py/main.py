02b88b4d6df3170e1c4be66e6c4ee088d137c493
kimd0nghun <37896242+kimd0nghun@users.noreply.github.com>
2022-01-01 22:36:22 +0900

    Add files via upload

--- src/B_16234py/main.py
+++ src/B_16234py/main.py
@@ -1,84 +1,50 @@
 import sys
+from collections import deque
 
+N,L,R = map(int, sys.stdin.readline().split())
+g =[]
+for  i in range(N):
+    g.append(list(map(int, sys.stdin.readline().split())))
+    
+gox=[0,1,0,-1]
+goy=[1,0,-1,0]
 
-def isPossible(x, y, size):
+def bfs(a,b,g, visit):
+    unit=[]
+    people =0
+    q = deque()
+    q.append((a,b))
+    unit.append((a,b))
+    people+=g[a][b]
+    visit[a][b] = True
+    while q:
+        x,y = q.popleft()
+        for i in range(4):
+            nx = x + gox[i]
+            ny = y + goy[i]
+            if 0 <= nx <N and 0<= ny<N and not visit[nx][ny]:
+                if L<= abs(g[nx][ny] - g[x][y]) <= R:
+                    q.append((nx,ny))
+                    unit.append((nx,ny))
+                    people += g[nx][ny]
+                    visit[nx][ny] = True
+    newPeople = people // len(unit)
+    for a,b in unit:
+        g[a][b] = newPeople
+    return True if len(unit) >= 2 else False
 
-    if x < 0 or y < 0 or x >= size or y >= size:
-        return False
-    return True
-
-
-def bfs(A, L, R):
-    gox = [0, 1, 0, -1]
-    goy = [1, 0, -1, 0]
-    size = len(A)
-
-    queue = []
-    baseCondition = False
-    Date = 0
-    while True:
-        c = [[0] * size for i in range(size)]
-        cc = [[0] * size for i in range(size)]
-
-        isChanged = False
-        if baseCondition:
-            print(Date - 1)
-            break
-        Date += 1
-        for i in range(size):
-            for j in range(size):
-                if not c[i][j]:
-                    queue.append((i, j))
-                person = 0
-                cnt = 0
-                while queue:
-                    x, y = queue.pop(0)
-                    for k in range(4):
-                        nx = x + gox[k]
-                        ny = y + goy[k]
-                        if isPossible(nx, ny, size):
-                            if not c[nx][ny]:
-                                one = abs(A[nx][ny] - A[x][y])
-
-                                if L <= one <= R:
-                                    if not c[x][y]:
-                                        person += A[x][y]
-                                        cnt += 1
-                                    c[x][y] = True
-                                    c[nx][ny] = True
-
-                                    cc[x][y] = j
-                                    cc[nx][ny] = j
-
-                                    person += A[nx][ny]
-                                    cnt += 1
-                                    queue.append((nx, ny))
-                                    isChanged = True
-                if cnt == 0:
-                    continue
-                for k in range(size):
-                    for q in range(size):
-                        boo = c[k][q]
-                        time = cc[k][q]
-                        if boo and time == j:
-                            A[k][q] = person // cnt
-                        # if c[k][q]:
-                        #     A[k][q] = person // cnt
-        if not isChanged:
-            baseCondition = True
-
-    # return 1
-
-
-N, L, R = map(int, sys.stdin.readline().split())
-A = [[0] * N for i in range(N)]
-
-
-for i in range(N):
-    temp = list(map(int, sys.stdin.readline().split()))
-    for j in range(N):
-        A[i][j] = temp[j]
-bfs(A, L, R)
-
-# for i in range(N):
-#     print(A[i])
+day =0
+while True:
+    visit = [[False] * N for _ in range(N)]
+    stop = True
+    for i in range(N):
+        for j in range(N):
+            if not visit[i][j]:
+                check = bfs(i,j,g,visit)
+                if check:
+                    stop = False
+    if stop:
+        break
+    else:
+        day+=1
+print(day)
