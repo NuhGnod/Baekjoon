01ccf7d04d8bb36955eaa4adb716fa66f1e02171
김동훈 <zhfptm12@naver.com>
2022-02-25 22:09:21 +0900

    22.02.25 10:09

--- src/B_20007py/main.py
+++ src/B_20007py/main.py
@@ -33,7 +33,6 @@ def cal():
             pX -= distance[i]
     print(day)
             
-
 N,M,X,Y = map(int, sys.stdin.readline().split())
 graph=[ [] for _ in range(N)]
 distance=[INF for _ in range(N)]
