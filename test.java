058aba6b3f0c647b1cdbc3ac2e92a607c729c3e3
Kimdonghun <12@gmail.com>
2022-03-14 19:27:06 +0900

    intellij test

--- test.java
+++ test.java
@@ -2,6 +2,6 @@ package baekjoon;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("hello intellij");
+        System.out.println("hello intellij!!");
     }
 }
