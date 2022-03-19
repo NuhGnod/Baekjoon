8a260ff5a11821a0ca0c9e2c8c5eaa047f0f8c7e
Kimdonghun <12@gmail.com>
2022-03-19 20:41:00 +0900

    22.3.19

---
+++ src/B_20040/Main.java
@@ -0,0 +1,12 @@
+package B_20040;
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+
+}
